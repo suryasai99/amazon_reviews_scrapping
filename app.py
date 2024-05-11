d9786d01603839b85790b920145d69a70fc40e40
surya sai kadali <suryasaikadali@suryas-MacBook-Air.local>
2024-05-11 08:15:10 +0200

    added cross_origin

--- app.py
+++ app.py
@@ -14,10 +14,12 @@ lg.basicConfig(level = lg.INFO,
 app = Flask(__name__)
 
 @app.route("/", methods = ['GET'])
+@cross_origin()
 def homepage():
     return render_template("index.html")
 
 @app.route("/review", methods = ['POST' , 'GET'])
+@cross_origin()
 def index():
     if request.method == 'POST':
         try:
